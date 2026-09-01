a8b9c802bb9414f5a4ef42a93a1ab7a9a0dcd89f
galvaolucas10 <galvaolucas943@gmail.com>
2026-09-01 19:58:50 -0300

    7parte

--- Overworld.js
+++ Overworld.js
@@ -1,44 +1,53 @@
 class Overworld {
-    constructor(config) {
-     this.element = config.element;
-     this.canvas = this.element.querySelector(".game-canvas");
-     this.ctx = this.canvas.getContext("2d");
-     this.map = null;
-    }
+  constructor(config) {
+    this.element = config.element;
+    this.canvas = this.element.querySelector(".game-canvas");
+    this.ctx = this.canvas.getContext("2d");
+    this.map = null;
+ }
 
-    startGameLoop() {
-      const step =  () => {
-       //Clear off the canvas
-       this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  startGameLoop() {
+    const step =  () => {
+      //Clear off the canvas
+      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
-       //Draw Lower layer
-       this.map.drawLowerImage(this.ctx);
-       
-       //Draw Game  Objects
-       Object.values(this.map.gameObjects).forEach(object => {
+      //Establish the camera person
+      const cameraPerson = this.map.gameObjects.hero;
+
+      //Update all objects
+      Object.values(this.map.gameObjects).forEach(object => {
         object.update({
-          arrow: this.directionInput.direction
+          arrow: this.directionInput.direction,
+          map: this.map,
         })
-        object.sprite.draw(this.ctx);
-       })
+      })
+
+      //Draw Lower layer
+      this.map.drawLowerImage(this.ctx, cameraPerson);
        
-       //Draw Upper layer
-       this.map.drawUpperImage(this.ctx);
+      //Draw Game  Objects
+      Object.values(this.map.gameObjects).forEach(object => {
+        object.sprite.draw(this.ctx, cameraPerson);
+      })
+       
+      //Draw Upper layer
+      this.map.drawUpperImage(this.ctx, cameraPerson);
 
-        requestAnimationFrame(() =>{
-         step();
-        })
-      }
-      step();
+      requestAnimationFrame(() =>{
+        step();
+      })
     }
+    step();
+ }
 
-    init() {
-    this.map = new OverworldMap(window.OverworldMap.DemoRoom);
+ init() {
+  this.map = new OverworldMap(window.OverworldMap.DemoRoom);
+  this.map.mountObjects();
 
-    this.directionInput = new DirectionInput();
-    this.directionInput.init();
-    this.directionInput.direction; //"down" 
+  this.directionInput = new DirectionInput();
+  this.directionInput.init();
+  this.directionInput.direction; //"down" 
 
-    this.startGameLoop();
-    }
+  this.startGameLoop();
+ }
 }
