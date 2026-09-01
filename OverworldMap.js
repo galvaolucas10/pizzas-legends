0514df073ea6c8454f2b90dec1d5a0fff738ee96
galvaolucas10 <galvaolucas943@gmail.com>
2026-08-31 21:38:24 -0300

    5parte

--- OverworldMap.js
+++ OverworldMap.js
@@ -28,7 +28,7 @@ window.OverworldMap = {
             x: utils.withGrid(5),
             y: utils.withGrid(6),
         }),
-        npc1: new Person({
+         npc1: new Person({
             x: utils.withGrid(7),
             y: utils.withGrid(9),
            src: "images/npc1.png"
